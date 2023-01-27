eae41acb3b69d03690ba20218731a0389e5e94f3
heeezin <gmlwls839@nate.com>
2023-01-27 20:39:44 +0900

    proxy세팅

--- backend/api-server.js
+++ backend/api-server.js
@@ -1,45 +1,53 @@
-// // import axios from 'axios'
+// import axios from 'axios'
 
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-// const bodyParser = require("body-parser");
-// const database = require("./database");
-// // const url = 'https://memo-app.herokuapp.com/'
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const database = require("./database");
 
-// // export const fetchPosts = () => axios.get(url)
-// // export const createPost = (newPost) => axios.post(url, newPost)
-// // export const updatePost = (id, updatedPost) => {
-// //   return axios.patch(`${url}/${id}`, updatedPost)
-// // }
-// // export const deletePost = (id) => axios.delete(`${url}/${id}`)
-// // export const likePost = (id) => axios.patch(`${url}/${id}/likePost`)
 
-// app.use(bodyParser.json());
-// app.use(express.static("dist"));
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+const URL = `${PROXY}/v1/search/book.json`;
+ 
+await = axios.get(URL, {
+    // ...
+}) ;
+// const url = 'https://memo-app.herokuapp.com/'
 
-// app.get("/api/memos", async (req, res) => {
-//   const result = await database.run("SELECT * FROM memos");
-//   res.send(result);
-// });
+// export const fetchPosts = () => axios.get(url)
+// export const createPost = (newPost) => axios.post(url, newPost)
+// export const updatePost = (id, updatedPost) => {
+//   return axios.patch(`${url}/${id}`, updatedPost)
+// }
+// export const deletePost = (id) => axios.delete(`${url}/${id}`)
+// export const likePost = (id) => axios.patch(`${url}/${id}/likePost`)
 
-// app.post("/api/memos", async (req, res) => {
-//   await database.run(`INSERT INTO memos (content) VALUES (?)`, [
-//     req.body.content,
-//   ]);
-//   const result = await database.run("SELECT * FROM memos");
-//   res.send(result);
-// });
+app.use(bodyParser.json());
+app.use(express.static("dist"));
 
-// app.put("/api/memos/:id", async (req, res) => {
-//   await database.run(`UPDATE memos SET content = ? WHERE id = ?`, [
-//     req.body.content,
-//     req.params.id,
-//   ]);
-//   const result = await database.run("SELECT * FROM memos");
-//   res.send(result);
-// });
+app.get("/api/memos", async (req, res) => {
+  const result = await database.run("SELECT * FROM memos");
+  res.send(result);
+});
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
+app.post("/api/memos", async (req, res) => {
+  await database.run(`INSERT INTO memos (content) VALUES (?)`, [
+    req.body.content,
+  ]);
+  const result = await database.run("SELECT * FROM memos");
+  res.send(result);
+});
+
+app.put("/api/memos/:id", async (req, res) => {
+  await database.run(`UPDATE memos SET content = ? WHERE id = ?`, [
+    req.body.content,
+    req.params.id,
+  ]);
+  const result = await database.run("SELECT * FROM memos");
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
